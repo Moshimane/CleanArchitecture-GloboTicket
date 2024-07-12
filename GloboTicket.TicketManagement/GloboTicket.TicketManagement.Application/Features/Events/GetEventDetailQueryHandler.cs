c9aa244fd16033d73bcfd3ed1491c4e6b3987ead
Moshimane <joy.moshimane@gmail.com>
2024-07-12 23:13:04 +0200

    add application project

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Application/Features/Events/GetEventDetailQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Features.Dtos;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+    public class GetEventDetailQueryHandler : IRequestHandler<GetEventDetailQuery, EventDetailDTO>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IAsyncRepository<Category> _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetEventDetailQueryHandler(IAsyncRepository<Event> eventRepository, IAsyncRepository<Category> categoryRepository, IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+        public async Task<EventDetailDTO> Handle(GetEventDetailQuery request, CancellationToken cancellationToken)
+        {
+            var @event = await _eventRepository.GetByIdAsync(request.Id);
+            var eventDetailDto = _mapper.Map<EventDetailDTO>(@event);
+
+            var category = await _categoryRepository.GetByIdAsync(@event.CategoryId);
+
+            eventDetailDto.Category = _mapper.Map<CategoryDTO>(category);
+
+            return eventDetailDto;
+        }
+    }
+}
---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Application/Features/Events/GetEventListQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Features.Dtos;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+    public class GetEventListQueryHandler : IRequestHandler<GetEventsListQuery, List<EventListDTO>>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public GetEventListQueryHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+
+
+        public async Task<List<EventListDTO>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+        {
+            var allEvents = (await _eventRepository.ListAllAsync()).OrderBy(x => x.Date);
+
+            return _mapper.Map<List<EventListDTO>>(allEvents);
+        }
+    }
+}
